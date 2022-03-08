69e4a466099ef7f29fd117088d16b65938e5d3ca
unknown <rud.hardianto@gmail.com>
2022-03-08 11:52:22 +0700

    setup layout dashboard

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,29 @@
+<div class="w-1/5 min-h-screen bg-gray-900 px-4 py-6">
+   <div class="mb-8">
+      <header class="font-medium px-2 uppercase text-xs text-gray-500">
+         Main
+      </header>
+      <a href="#" class="block text-gray-200 px-4 py-2">Dashboard</a>
+   </div>
+   <div class="mb-8">
+      <header class="font-medium px-2 uppercase text-xs text-gray-500">
+         Playlists
+      </header>
+      <a href="#" class="block text-gray-200 px-4 py-2">Create</a>
+      <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
+   </div>
+   <div class="mb-8">
+      <header class="font-medium px-2 uppercase text-xs text-gray-500">
+         Category
+      </header>
+      <a href="#" class="block text-gray-200 px-4 py-2">Create</a>
+      <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
+   </div>
+   <div class="mb-8">
+      <header class="font-medium px-2 uppercase text-xs text-gray-500">
+         Users
+      </header>
+      <a href="#" class="block text-gray-200 px-4 py-2">Create</a>
+      <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
+   </div>
+</div>
