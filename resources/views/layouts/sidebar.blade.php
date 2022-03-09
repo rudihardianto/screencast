2892b70364f2579c4977796f4d00a526932e75cc
unknown <rud.hardianto@gmail.com>
2022-03-09 10:13:45 +0700

    set role with spatie

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,29 +1,50 @@
 <div class="w-1/5 min-h-screen bg-gray-900 px-4 py-6">
+   {{-- Dashboard --}}
    <div class="mb-8">
       <header class="font-medium px-2 uppercase text-xs text-gray-500">
          Main
       </header>
       <a href="#" class="block text-gray-200 px-4 py-2">Dashboard</a>
    </div>
-   <div class="mb-8">
-      <header class="font-medium px-2 uppercase text-xs text-gray-500">
-         Playlists
-      </header>
-      <a href="#" class="block text-gray-200 px-4 py-2">Create</a>
-      <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
-   </div>
-   <div class="mb-8">
-      <header class="font-medium px-2 uppercase text-xs text-gray-500">
-         Category
-      </header>
-      <a href="#" class="block text-gray-200 px-4 py-2">Create</a>
-      <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
-   </div>
-   <div class="mb-8">
-      <header class="font-medium px-2 uppercase text-xs text-gray-500">
-         Users
-      </header>
-      <a href="#" class="block text-gray-200 px-4 py-2">Create</a>
-      <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
-   </div>
+
+   {{-- Playlists --}}
+   @if (Auth::user()->can('create playlists'))
+      <div class="mb-8">
+         <header class="font-medium px-2 uppercase text-xs text-gray-500">
+            Playlists
+         </header>
+         <a href="#" class="block text-gray-200 px-4 py-2">Create</a>
+         <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
+      </div>
+   @endif
+
+   {{-- Tags --}}
+   @if (Auth::user()->can('create tags'))
+      <div class="mb-8">
+         <header class="font-medium px-2 uppercase text-xs text-gray-500">
+            Tags
+         </header>
+         <a href="#" class="block text-gray-200 px-4 py-2">Create</a>
+         <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
+      </div>
+   @endif
+
+   {{-- Users --}}
+   @can('show users')
+      <div class="mb-8">
+         <header class="font-medium px-2 uppercase text-xs text-gray-500">
+            Users
+         </header>
+         <a href="#" class="block text-gray-200 px-4 py-2">Table</a>
+      </div>
+   @endcan
+
+   {{-- Logout --}}
+   <form method="POST" action="{{ route('logout') }}">
+      @csrf
+      <a class="block text-red-600 px-4 py-2" href="route('logout')" onclick="event.preventDefault();
+                       this.closest('form').submit();">
+         {{ __('Log Out') }}
+      </a>
+   </form>
 </div>
