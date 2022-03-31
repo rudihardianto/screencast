44a725a6cc46a99258a82ec312e2a9f8bf74aa2b
unknown <rud.hardianto@gmail.com>
2022-03-31 09:20:47 +0700

    playlist & video endpoint

--- app/Models/Screencast/Playlist.php
+++ app/Models/Screencast/Playlist.php
@@ -41,7 +41,7 @@ class Playlist extends Model
       return $this->belongsTo(User::class);
    }
 
-   public function usersPurchases()
+   public function purchasedBy()
    {
       return $this->belongsToMany(User::class, 'purchased_playlist', 'playlist_id', 'user_id');
    }
